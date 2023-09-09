92aab4a076d0f0c33f3d206cb71e47eb422f7227
Mocca24 <signupcyn@gmail.com>
2023-09-09 19:52:07 +0300

    code from sandbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
+import React from "react";
+import "./Weather.css";
 
-import './App.css';
-import Weather from './Weather';
+export default function Weather() {
+  let weatherData = {
+    city: "Montevideo",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 65,
+    wind: 29
+  };
 
-export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-
-        <Weather />
-        <footer>
-          This project was coded by {" "}
-          <a href="https://www.linkedin.com/in/andria-c/" target="_blank" rel="noreferrer"> Andria
-          </a> and is {" "}
-          <a href="https://github.com/andria-coder/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
-        </footer>
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,68 @@
 import React from "react";
 import "./Weather.css";
 
-export default function Weather(){
-  return(
+export default function Weather() {
+  let weatherData = {
+    city: "Montevideo",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 65,
+    wind: 29
+  };
+
+  return (
     <div className="Weather">
-      <form>
+      <form className="mb-3">
         <div className="row">
           <div className="col-9">
             <input
-              type="Search"
-              placeholder="Enter a city..."
-              className="form-control"/>
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoComplete="off"
+            />
           </div>
           <div className="col-3">
             <input
-            type="Submit"
-            value="Search"
-            className="btn btn-primary w-100" />
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
-       <h1>Cologne</h1>
-       <ul>
-          <li>Wednesday 07:00</li>
-          <li>Sunny</li>
-       </ul>
-       <div className="row">
-          <div className="col-6">
-            <div className="clearfix">
-              <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                    alt="sunny"
-                    className="float-left" />
-              <span className="float-left">
-                <span className="temperature">20</span><span className="unit">°C</span>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
               </span>
             </div>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>
-                Precipitation: 5%
-              </li>
-              <li>Humidity 60%</li>
-              <li>Wind: 13 km/h</li>
-            </ul>
-          </div>
-       </div>
-
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
