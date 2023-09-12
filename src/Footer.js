c664f46920713de2045b9d629c366096ededca79
Mocca24 <signupcyn@gmail.com>
2023-09-12 08:59:27 +0300

    footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+export default function gitUrl() {
+  return (
+    <div>
+     <p>
+        <a
+          href="https://github.com/Mocca24/react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>
+        , by Cynthia Alvez
+      </p>
+    </div>
+  );
+}
