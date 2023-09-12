971a7bb819e7bea676bdbacabd6e79822062b009
Mocca24 <signupcyn@gmail.com>
2023-09-12 09:15:03 +0300

    footer css

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Footer.css"
 
 export default function Footer() {
   return (
-    <div>
+    <div className="Git">
      <p>
         <a
           href="https://github.com/Mocca24/react-app"
